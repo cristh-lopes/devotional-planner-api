516dd1239de58254dbc0f76caf2b32841b576cae
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-11-19 16:53:47 -0300

    refactor: plan repository with planEnum

--- src/modules/plan/plan.repository.ts
+++ src/modules/plan/plan.repository.ts
@@ -1,12 +1,13 @@
 import chronological from "./data/chronological.json";
+import { PlanEnum } from "./plan.enum";
 import { Plan } from "./plan.types";
 
-const planMap: Record<string, Plan> = {
+const planMap: Record<PlanEnum, Plan> = {
   chronological: chronological as Plan,
 };
 
 export class PlanRepository {
-  static get(plan: string): Plan {
+  static get(plan: PlanEnum): Plan {
     const p = planMap[plan];
     if (!p) throw new Error("Plano não suportado");
     return p;
