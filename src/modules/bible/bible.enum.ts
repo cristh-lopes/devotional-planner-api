5f68555e3946a3668ba8e7f1362eceae54791499
Cristhian Lopes <dev.cristh.lopes@gmail.com>
2025-12-03 19:28:07 -0300

    feat: novas versões da bíblia disponiveis no sistema

--- src/modules/bible/bible.enum.ts
+++ src/modules/bible/bible.enum.ts
@@ -1,5 +1,9 @@
 export enum VersionEnum {
   NVI = "nvi",
+  NBV = "nbv",
+  NVT = "nvt",
+  NTLH = "ntlh",
+  NAA = "naa",
   ACF = "acf",
   AA = "aa",
 }
--- src/modules/bible/bible.repository.ts
+++ src/modules/bible/bible.repository.ts
@@ -1,12 +1,20 @@
 import { Bible } from "./bible.types";
 import { VersionEnum } from "./bible.enum";
 
+import nvt from "./data/nvt.json";
+import ntlh from "./data/ntlh.json";
+import nbv from "./data/nbv.json";
+import naa from "./data/naa.json";
 import nvi from "./data/nvi.json";
 import acf from "./data/acf.json";
 import aa from "./data/aa.json";
 
 const bibleMap: Record<VersionEnum, Bible> = {
   [VersionEnum.NVI]: nvi as Bible,
+  [VersionEnum.NTLH]: ntlh as Bible,
+  [VersionEnum.NBV]: nbv as Bible,
+  [VersionEnum.NVT]: nvt as Bible,
+  [VersionEnum.NAA]: naa as Bible,
   [VersionEnum.ACF]: acf as Bible,
   [VersionEnum.AA]: aa as Bible,
 };
